f9be401904e55ea7309cc2bc0a736f7b8543db22
Cristian Gomez <cgomez@sfisas.com>
2025-04-26 12:02:21 -0500

    add++

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ from apscheduler.schedulers.background import BackgroundScheduler
 
 app = FastAPI()
 
-origins = ["https://forms.sfisas.com.co/"]
+origins = ["https://forms.sfisas.com.co"]
 
 app.add_middleware(
     CORSMiddleware,
